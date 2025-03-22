f59289c594c3d1915fc3b79b8a832dc9608704a6
chati112 <133596038+chati112@users.noreply.github.com>
2025-03-22 21:55:50 +0100

    second commit

---
+++ TickIT/AdminView.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace TickIT
+{
+    partial class AdminView
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView_Users = new System.Windows.Forms.DataGridView();
+            this.lbl_FullName = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_Dodaj = new System.Windows.Forms.Button();
+            this.btn_Usun = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Users)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView_Users
+            // 
+            this.dataGridView_Users.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Users.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView_Users.Name = "dataGridView_Users";
+            this.dataGridView_Users.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView_Users.Size = new System.Drawing.Size(430, 426);
+            this.dataGridView_Users.TabIndex = 0;
+            // 
+            // lbl_FullName
+            // 
+            this.lbl_FullName.AutoSize = true;
+            this.lbl_FullName.Location = new System.Drawing.Point(488, 37);
+            this.lbl_FullName.Name = "lbl_FullName";
+            this.lbl_FullName.Size = new System.Drawing.Size(55, 13);
+            this.lbl_FullName.TabIndex = 1;
+            this.lbl_FullName.Text = "Full name:";
+            this.lbl_FullName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(617, 132);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(491, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(247, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(491, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(491, 132);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(617, 90);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(121, 20);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(488, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Phone";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(488, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Email adress:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(614, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(614, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "label4";
+            // 
+            // btn_Dodaj
+            // 
+            this.btn_Dodaj.Location = new System.Drawing.Point(491, 174);
+            this.btn_Dodaj.Name = "btn_Dodaj";
+            this.btn_Dodaj.Size = new System.Drawing.Size(247, 35);
+            this.btn_Dodaj.TabIndex = 11;
+            this.btn_Dodaj.Text = "Dodaj";
+            this.btn_Dodaj.UseVisualStyleBackColor = true;
+            // 
+            // btn_Usun
+            // 
+            this.btn_Usun.Location = new System.Drawing.Point(491, 229);
+            this.btn_Usun.Name = "btn_Usun";
+            this.btn_Usun.Size = new System.Drawing.Size(247, 35);
+            this.btn_Usun.TabIndex = 12;
+            this.btn_Usun.Text = "Usuń";
+            this.btn_Usun.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Usun);
+            this.Controls.Add(this.btn_Dodaj);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lbl_FullName);
+            this.Controls.Add(this.dataGridView_Users);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Users)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView_Users;
+        private System.Windows.Forms.Label lbl_FullName;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_Dodaj;
+        private System.Windows.Forms.Button btn_Usun;
+    }
+}
+
---
+++ TickIT/AdminView.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TickIT
+{
+    public partial class AdminView : Form
+    {
+        public AdminView()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshDataGridView();
+        }
+
+        private void RefreshDataGridView()
+        {
+            string connectionString = "Data Source=TickIT.db;Version=3;";
+
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string query = "SELECT UserID, FullName, Email, Phone, Role FROM Users"; // Pobranie wszystkich użytkowników
+
+                    using (SQLiteDataAdapter adapter = new SQLiteDataAdapter(query, conn))
+                    {
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+
+                        dataGridView_Users.DataSource = dt;
+                        FormatDataGridView(); // Ponowne formatowanie
+                    }
+                }
+            }
+            catch (SQLiteException ex2)
+            {
+                MessageBox.Show("SQLite Error: " + ex2.Message);
+            }
+            catch (Exception ex1)
+            {
+                MessageBox.Show("Error: " + ex1.Message);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormatDataGridView()
+        {
+            // Zablokowanie edycji
+            dataGridView_Users.ReadOnly = true;
+
+            // Ukrycie pustego wiersza na końcu
+            dataGridView_Users.AllowUserToAddRows = false;
+            dataGridView_Users.AllowUserToDeleteRows = false;
+
+            // Auto-dopasowanie szerokości kolumn do zawartości
+            dataGridView_Users.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            // Us   tawienie nagłówków na pogrubione
+            dataGridView_Users.ColumnHeadersDefaultCellStyle.Font = new Font("Arial", 10, FontStyle.Bold);
+            dataGridView_Users.ColumnHeadersDefaultCellStyle.BackColor = Color.LightGray;
+            dataGridView_Users.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            // Wyrównanie tekstu w komórkach do środka
+            dataGridView_Users.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            // Ustawienie wysokości wierszy
+            dataGridView_Users.RowTemplate.Height = 30;
+
+            // Zmiana koloru naprzemiennych wierszy dla lepszej czytelności
+            dataGridView_Users.AlternatingRowsDefaultCellStyle.BackColor = Color.LightBlue;
+
+            // Ustawienie obramowania komórek
+            dataGridView_Users.CellBorderStyle = DataGridViewCellBorderStyle.Single;
+            dataGridView_Users.GridColor = Color.Black;
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
